2e038579956ea4d9e162135ddb0e3cb08882f441
LjsOks <lajos.okos@gmail.com>
2019-02-11 00:10:38 +0100

    Corrected directory name.

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from wsgiref.simple_server import make_server
 
 # obtain the WSGI request dispatcher
 from roundup.cgi.wsgi_handler import RequestDispatcher
-tracker_home = 'demo'
+tracker_home = 'tpm'
 application = RequestDispatcher(tracker_home)
 
 httpd = make_server('', 8080, application)
